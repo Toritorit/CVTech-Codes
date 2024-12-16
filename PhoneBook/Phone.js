0621036ef22a91536dc1b2a90fca49ab482d2cc4
Toritorit <tori.mckissick@cvtechonline.net>
2024-12-16 11:02:45 -0600

    Add files via upload

---
+++ PhoneBook/Phone.js
@@ -0,0 +1,14 @@
+function myfunction (){
+let First = document.getElementById("First").value;
+let Last = document.getElementById("Last").value;
+let Phone = document.getElementById("Phone").value;
+let inN = document.getElementById("inN")
+let inNU = document.getElementById("inNU")
+
+
+
+inN.innerHTML += First + Last + "<hr/>"
+inNU.innerHTML += Phone + "<hr/>"
+
+
+}
