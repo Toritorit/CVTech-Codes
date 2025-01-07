24387a844e9f1575015e8d83fc4703fbb8b401f0
Toritorit <tori.mckissick@cvtechonline.net>
2025-01-07 10:47:59 -0600

    Add files via upload

---
+++ Exponets/Exponents.js
@@ -0,0 +1,14 @@
+    
+    
+function Ex() {
+
+let Base = document.getElementById("Base").value;
+let Expo = document.getElementById("Expo").value;       
+let Result = document.getElementById("Result");
+
+Result.innerHTML += (Base) ** Expo;
+
+
+
+
+}
