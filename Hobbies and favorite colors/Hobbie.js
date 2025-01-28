f5c884278aab6eded7aa3d82bc0007b874ebbeda
Toritorit <tori.mckissick@cvtechonline.net>
2025-01-28 08:56:58 -0600

    Add files via upload

---
+++ Hobbies and favorite colors/Hobbie.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+function Colorswitch () {
+    let input1 = document.querySelector("#input1").value
+    let box1 = document.getElementById("box1")
+    box1.style.backgroundColor = input1
+}
+
+function Colorswitch2 () {
+    let input2 = document.querySelector("#input2").value
+    let box2 = document.getElementById("box2")
+    box2.style.backgroundColor = input2
+}
+
+function Colorswitch3 () {
+    let input3 = document.querySelector("#input3").value
+    let box3 = document.getElementById("box3")
+    box3.style.backgroundColor = input3
+}
+
+function Colorswitch4 () {
+    let input4 = document.querySelector("#input4").value
+    let box4 = document.getElementById("box4")
+    box4.style.backgroundColor = input4
+}
+
+function Colorswitch5 () {
+    let input5 = document.querySelector("#input5").value
+    let box5 = document.getElementById("box5")
+    box5.style.backgroundColor = input5
+}
+
